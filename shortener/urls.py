97429ec844eb5a19888b676cf2d1010db78ceacb
sachinbudhamagar <sachinbmagar19@gmail.com>
2026-02-13 23:03:37 +0900

    Add registration template and URL routing for user registration

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from django.urls import path
+from . import views
 
 # Create your tests here.
+urlpatterns = [
+    path("register/", views.register, name="register"),
+]
